eb409b61b10f7568e927c8b47ab819ac5d16b263
Otávio Palma <otaviopalmapsn@gmail.com>
2025-12-18 20:20:57 -0300

    db alteracoes

--- db.js
+++ db.js
@@ -1,13 +1,16 @@
-const mysql = require("mysql2/promise")
-
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  host: process.env.MYSQLHOST || process.env.MYSQL_HOST,
+  user: process.env.MYSQLUSER || process.env.MYSQL_USER,
+  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT || process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
 })
 
-module.exports = pool
+const pool = require('./db');
+
+// Teste de conexão
+pool.query('SELECT 1')
+  .then(() => console.log('MySQL conectado com sucesso!'))
+  .catch(err => console.error('Erro ao conectar MySQL:', err));
