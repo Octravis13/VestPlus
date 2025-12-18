c5e0c0b5990e78739f94a76e44993dd66555f97e
Otávio Palma <otaviopalmapsn@gmail.com>
2025-12-18 20:01:28 -0300

    ajuste dbjs3

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 const mysql = require("mysql2/promise")
 
-const pool = mysql.createPool(process.env.MYSQL_URL)
+const {
+  MYSQLUSER,
+  MYSQL_ROOT_PASSWORD,
+  RAILWAY_PRIVATE_DOMAIN,
+  MYSQL_DATABASE,
+} = process.env
+
+if (!MYSQLUSER || !MYSQL_ROOT_PASSWORD || !MYSQL_DATABASE) {
+  throw new Error("❌ Variáveis MySQL incompletas")
+}
+
+const MYSQL_URL = `mysql://${MYSQLUSER}:${MYSQL_ROOT_PASSWORD}@${RAILWAY_PRIVATE_DOMAIN}:3306/${MYSQL_DATABASE}`
+
+const pool = mysql.createPool(MYSQL_URL)
 
 module.exports = pool
-
-pool.getConnection()
-  .then(conn => {
-    console.log("✅ MySQL conectado via MYSQL_URL")
-    conn.release()
-  })
-  .catch(err => {
-    console.error("❌ Erro MySQL:", err)
-  })
